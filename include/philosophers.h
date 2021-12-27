a4b04b72d3c4503ac0683a261161348dcb181aea
caio-vinicius <contato@caiovinicius.dev.br>
2021-12-26 22:45:02 -0300

    handling the arguments and storing them in a data structure

--- include/philosophers.h
+++ include/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: csouza-f <caio@42sp.org.br>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/26 10:16:18 by csouza-f          #+#    #+#             */
-/*   Updated: 2021/12/26 10:52:44 by csouza-f         ###   ########.fr       */
+/*   Updated: 2021/12/26 22:23:26 by csouza-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+
+struct s_args {
+	unsigned int number_of_philosophers;
+	unsigned int time_to_die;
+	unsigned int time_to_eat;
+	unsigned int time_to_sleep;
+	unsigned int number_of_times_each_philosopher_must_eat;
+};
 
 #endif
